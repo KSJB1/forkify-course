d68a61ec01c78e197ca197017de265ced45f157b
KSJB1 <karl.bessell@gmail.com>
2023-06-18 20:09:27 +0100

    change made

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -2,7 +2,7 @@ import View from "./View";
 
 // import icons from '../img/icons.svg'; // Parcel 1
 import icons from "url:../../img/icons.svg"; // Parcel 2
-import { numberToFraction } from "./views/helpers.js";
+import { numberToFraction } from "./helpers.js";
 
 class RecipeView extends View {
   _parentElement = document.querySelector(".recipe");
